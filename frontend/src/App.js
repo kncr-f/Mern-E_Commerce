fb3181556f310bd1fbef9bd31ed160501558e132
Fatih Kincir <kincirfatih@gmail.com>
2022-03-06 21:42:33 +0100

    React-Bootstrap Setup, Header_Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to E-Commerce</h1>
 
-      <h1>Welcome to E-Commerce</h1>
+        </Container>
+      </main>
+
+      <Footer />
     </>
   );
 }
