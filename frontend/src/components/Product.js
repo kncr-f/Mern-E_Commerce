0430b7e4326ff7ad7dc1626398f86709d5fb22ef
Fatih Kincir <kincirfatih@gmail.com>
2022-03-08 00:27:14 +0100

    implementing react router and creating rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React from 'react';
+import { Link } from "react-router-dom";
+import { Card } from 'react-bootstrap';
+import Rating from "./Rating";
 
 function Product(props) {
     return (
         <Card className='my-3 p-3 rounded'>
-            <a href={`/product/${props.product._id}`}>
+            <Link to={`/product/${props.product._id}`}>
                 <Card.Img src={props.product.image} variant="top" />
-            </a>
+            </Link>
 
             <Card.Body>
-                <a href={`/product/${props.product._id}`}>
+                <Link to={`/product/${props.product._id}`}>
                     <Card.Title as='div'>
                         <strong>
                             {props.product.name}
                         </strong>
                     </Card.Title>
-                </a>
+                </Link>
                 <Card.Text as='div'>
-                    <div className="my-3">
-                        {props.product.rating} from {props.product.numReviews} reviews
-                    </div>
+                    <Rating value={props.product.rating}
+                        text={`${props.product.numReviews} reviews`}
+                    />
                 </Card.Text>
                 <Card.Text as='h3'> ${props.product.price}</Card.Text>
             </Card.Body>
