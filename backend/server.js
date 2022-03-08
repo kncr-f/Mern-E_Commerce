d8fd94777dd5da7b63e66b6a4f167fa29999b26c
Fatih Kincir <kincirfatih@gmail.com>
2022-03-08 17:19:27 +0100

    MongoDB Atlas & Compass Setup, connecting the database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
