e5f93825e6be2c2f842a85f77d8fbdaf9b227dbf
Fatih Kincir <kincirfatih@gmail.com>
2022-03-14 22:30:23 +0100

    Creating recuder and action for productList

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
                     <LinkContainer to="/">
-                        <Navbar.Brand >FunnyShoppy</Navbar.Brand>
+                        <Navbar.Brand >EasyShopping</Navbar.Brand>
                     </LinkContainer>
 
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
