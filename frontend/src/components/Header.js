6f172d2fdc4309f0e04513e679693ff90500a56d
Fatih Kincir <kincirfatih@gmail.com>
2022-03-28 20:43:03 +0200

    logout functionality added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { useSelector, useDispatch } from 'react-redux';
+
+import { logout } from '../actions/userActions';
 
 const Header = () => {
+    const userLogin = useSelector(state => state.userLogin);
+    const { userInfo } = userLogin;
+
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        console.log("logout");
+        dispatch(logout())
+
+    }
+
+
     return (
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
@@ -18,17 +33,32 @@ const Header = () => {
                                 <Nav.Link > <i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
 
                             </LinkContainer>
-                            <LinkContainer to="/login">
-                                <Nav.Link ><i className='fas fa-user'></i>Sign In</Nav.Link>
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id="user_name">
+                                    <LinkContainer to="/profile">
+                                        <NavDropdown.Item> Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={handleLogout}>
+                                        Logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to="/login">
+                                    <Nav.Link ><i className='fas fa-user'></i>Sign In</Nav.Link>
 
-                            </LinkContainer>
+                                </LinkContainer>
+
+                            )
+
+                            }
+
 
 
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
-            </Navbar>
-        </header>
+            </Navbar >
+        </header >
     )
 }
 
