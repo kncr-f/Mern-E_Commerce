5b67b53cd131143e6a52bcc5736ec1d776bad694
Fatih Kincir <kincirfatih@gmail.com>
2022-04-01 12:44:49 +0200

    create & update product endpoints created

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -55,8 +55,67 @@ const deleteProduct = asyncHandler(async (req, res) => {
 
 });
 
+
+// @desc Create a product
+// @route POST /api/products
+// @access Public/Admin
+
+
+const createProduct = asyncHandler(async (req, res) => {
+    const product = new Product({
+        name: "sample name",
+        price: 0,
+        user: req.user._id,
+        image: "/images/sample.jpg",
+        brand: "Sample brand",
+        category: "Sample category",
+        countInStock: 0,
+        numReviews: 0,
+        description: "Sample description"
+    });
+
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct)
+
+});
+
+
+
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access Public/Admin
+
+
+const updateProduct = asyncHandler(async (req, res) => {
+
+    const { name, price, image, category, countInStock, description, brand } = req.body;
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        product.name = name;
+        product.price = price;
+        product.image = image;
+        product.category = category;
+        product.countInStock = countInStock;
+        product.description = description;
+        product.brand = brand;
+
+        const updatedProduct = await product.save();
+        res.json(updatedProduct);
+
+    } else {
+        res.status(404);
+        throw new Error
+    }
+
+
+});
+
 export {
     getProducts,
     getProductById,
-    deleteProduct
+    deleteProduct,
+    createProduct,
+    updateProduct,
+
 }
