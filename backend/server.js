f99475fee3e150bfb7b2336c5d9c8373e65f5626
Fatih Kincir <kincirfatih@gmail.com>
2022-04-09 10:33:43 +0200

    Prepare for heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,6 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("API is running....😤  ")
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -34,7 +31,18 @@ app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 
 const __dirname = path.resolve();
+
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")));
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running....😤  ")
+    });
+}
 
 
 app.use(notFound);
